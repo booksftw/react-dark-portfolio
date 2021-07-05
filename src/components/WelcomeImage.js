788f50f647d20cedfa93d257d25a05ba498d84d6
NicholasZ <nzachary46@gmail.com>
2021-07-05 16:22:34 -0700

    first version of animations implemented

--- src/components/WelcomeImage.js
+++ src/components/WelcomeImage.js
@@ -1,13 +1,117 @@
 import React from "react";
 import classes from "./WelcomeImage.module.css";
 import meImage from "../assets/me-blackandwhite.png";
+import meSvg from "../assets/me_svg.svg";
+
+import Particles from "react-particles-js";
 
 const WelcomeImage = (props) => {
   return (
     <div>
+      <Particles
+        height="100vh"
+        params={{
+          particles: {
+            number: {
+              value: 100,
+              density: {
+                enable: true,
+                value_area: 1500,
+              },
+            },
+            line_linked: {
+              enable: true,
+              opacity: 0.02,
+            },
+            move: {
+              direction: "right",
+              speed: 0.5,
+            },
+            size: {
+              value: 1,
+            },
+            opacity: {
+              anim: {
+                enable: true,
+                speed: 1,
+                opacity_min: 0.05,
+              },
+            },
+          },
+          interactivity: {
+            events: {
+              onclick: {
+                enable: true,
+                mode: "push",
+              },
+            },
+            modes: {
+              push: {
+                particles_nb: 1,
+              },
+            },
+          },
+          retina_detect: true,
+        }}
+      />
+      {/* <Particles
+        params={{
+          particles: {
+            number: {
+              value: 160,
+              density: {
+                enable: false,
+              },
+            },
+            size: {
+              value: 3,
+              random: true,
+              anim: {
+                speed: 4,
+                size_min: 0.3,
+              },
+            },
+            line_linked: {
+              enable: false,
+            },
+            move: {
+              random: true,
+              speed: 1,
+              direction: "top",
+              out_mode: "out",
+            },
+          },
+          interactivity: {
+            events: {
+              onhover: {
+                enable: true,
+                mode: "bubble",
+              },
+              onclick: {
+                enable: true,
+                mode: "repulse",
+              },
+            },
+            modes: {
+              bubble: {
+                distance: 250,
+                duration: 2,
+                size: 0,
+                opacity: 0,
+              },
+              repulse: {
+                distance: 400,
+                duration: 4,
+              },
+            },
+          },
+        }}
+      /> */}
       <img className={classes.welcomeImage} src={meImage} alt="" />
     </div>
   );
 };
 
 export default WelcomeImage;
+
+// https://images.theconversation.com/files/243439/original/file-20181101-83635-1xcrr39.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=900.0&fit=crop
