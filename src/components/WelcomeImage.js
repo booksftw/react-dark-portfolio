55f47b6c0e845942f7318526faab857ddabd4e22
NicholasZ <nzachary46@gmail.com>
2021-07-05 16:31:43 -0700

    update animation

--- src/components/WelcomeImage.js
+++ src/components/WelcomeImage.js
@@ -42,7 +42,7 @@ const WelcomeImage = (props) => {
             events: {
               onclick: {
                 enable: true,
-                mode: "push",
+                mode: "bubble",
               },
             },
             modes: {
@@ -54,59 +54,6 @@ const WelcomeImage = (props) => {
           retina_detect: true,
         }}
       />
-      {/* <Particles
-        params={{
-          particles: {
-            number: {
-              value: 160,
-              density: {
-                enable: false,
-              },
-            },
-            size: {
-              value: 3,
-              random: true,
-              anim: {
-                speed: 4,
-                size_min: 0.3,
-              },
-            },
-            line_linked: {
-              enable: false,
-            },
-            move: {
-              random: true,
-              speed: 1,
-              direction: "top",
-              out_mode: "out",
-            },
-          },
-          interactivity: {
-            events: {
-              onhover: {
-                enable: true,
-                mode: "bubble",
-              },
-              onclick: {
-                enable: true,
-                mode: "repulse",
-              },
-            },
-            modes: {
-              bubble: {
-                distance: 250,
-                duration: 2,
-                size: 0,
-                opacity: 0,
-              },
-              repulse: {
-                distance: 400,
-                duration: 4,
-              },
-            },
-          },
-        }}
-      /> */}
       <img className={classes.welcomeImage} src={meImage} alt="" />
     </div>
   );
